614154571f80c3a03e0639ccccdff8194a5b4071
x33zp <zubbypeculiar@gmail.com>
2024-08-13 17:27:41 +0100

    added new methods: count and get to dbstorge and file_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,3 +74,22 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
+
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        if cls not in classes.values():
+            return None
+        
+        all_cls = models.storage.all(cls)
+
+        for value in all_cls.values():
+            if value.id == id:
+                return value
+    
+    def count(self, cls=None):
+        """Count objects"""
+        all_obj = self.all(cls)
+
+        if cls in classes.values():
+            all_obj = self.all(cls)
+        return len(all_obj)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,6 +3,7 @@
 Contains the FileStorage class
 """
 
+import models
 import json
 from models.amenity import Amenity
 from models.base_model import BaseModel
@@ -68,3 +69,22 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """A method to retrieve one object"""
+        if cls not in classes.values():
+            return None
+        
+        all_cls = models.storage.all(cls)
+
+        for value in all_cls.values():
+            if value.id == id:
+                return value
+    
+    def count(self, cls=None):
+        """Count objects"""
+        all_obj = self.all(cls)
+
+        if cls in classes.values():
+            all_obj = self.all(cls)
+        return len(all_obj)
