4772ca2ba632531b69fef90360ee24a20ea49eff
x33zp <zubbypeculiar@gmail.com>
2024-08-13 18:46:32 +0100

    pycodestyle fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -8,10 +8,12 @@ app = Flask(__name__)
 
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def close_db(error):
     """ Close Storage """
     storage.close()
+
 
 if __name__ == '__main__':
     host = environ.get('HBNB_API_HOST')
