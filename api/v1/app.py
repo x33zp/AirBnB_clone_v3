f114d1e379edf8f2ea0568a796cb636c5247306d
x33zp <zubbypeculiar@gmail.com>
2024-08-16 00:26:41 +0100

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,28 +1,43 @@
 #!/usr/bin/python3
-"""Flask App"""
-from flask import Flask
+"""
+This module contains the principal application
+"""
 from models import storage
 from api.v1.views import app_views
+from flask import Flask, make_response, jsonify
+from os import getenv
+from flask_cors import CORS
+from flasgger import Swagger
 
 app = Flask(__name__)
-
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ close storage """
+def close_db(obj):
+    """ calls methods close() """
     storage.close()
 
 
-if __name__ == '__main__':
-    """Main function"""
-    host = environ.get('HBNB_API_HOST')
-    port = environ.get('HBNB_API_PORT')
+@app.errorhandler(404)
+def page_not_foun(error):
+    """ Loads a custom 404 page not found """
+    return make_response(jsonify({"error": "Not found"}), 404)
 
-    if not host:
-        host = '0.0.0.0'
-    if not port:
-        port = '5000'
-    app.run(host=host, port=port, threaded=True)
 
+app.config['SWAGGER'] = {
+    'title': 'AirBnB clone - RESTful API',
+    'description': 'This is the api that was created for the hbnb restful api project,\
+    all the documentation will be shown below',
+    'uiversion': 3}
+
+Swagger(app)
+
+if __name__ == "__main__":
+
+    host = getenv('HBNB_API_HOST', default='0.0.0.0')
+    port = getenv('HBNB_API_PORT', default=5000)
+
+    app.run(host, int(port), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,21 +1,28 @@
 #!/usr/bin/python3
-"""Index"""
+"""
+This module contains endpoint(route) status
+"""
+from models import storage
+from flask import Flask
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import jsonify
 
 
-@app.views.route('/status', strict_slashes=False)
+@app_views.route('/status', strict_slashes=False)
 def status():
-    """Returns a JSON status"""
-    return jsonify({"status": "OK"}
+    """
+    Returns a JSON status
+    """
+    return jsonify({"status": "OK"})
 
 
-@app.views.route('/stats', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def count():
-    """Retrieves the number of each object by type
+    """
+    Retrieves the number of each objects by type
     """
     return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("city"),
+                    "cities": storage.count("City"),
                     "places": storage.count("Place"),
                     "reviews": storage.count("Review"),
                     "states": storage.count("State"),
