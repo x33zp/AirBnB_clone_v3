f7ca532a7ff6451b00a6edcf3838097f60c44829
x33zp <zubbypeculiar@gmail.com>
2024-08-16 00:22:55 +0100

    Added: count method

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -7,4 +7,16 @@ from flask import Flask, jsonify
 @app.views.route('/status', strict_slashes=False)
 def status():
     """Returns a JSON status"""
-    return jsonify({"status": "OK"})
+    return jsonify({"status": "OK"}
+
+
+@app.views.route('/stats', strict_slashes=False)
+def count():
+    """Retrieves the number of each object by type
+    """
+    return jsonify({"amenities": storage.count("Amenity"),
+                    "cities": storage.count("city"),
+                    "places": storage.count("Place"),
+                    "reviews": storage.count("Review"),
+                    "states": storage.count("State"),
+                    "users": storage.count("User")})
