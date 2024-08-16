d45ef5965f972514d0e163feb92d2e91c9e04167
x33zp <zubbypeculiar@gmail.com>
2024-08-16 01:47:27 +0100

    pycodestyle --fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,14 +7,15 @@ from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
+
 @app.teardown_appcontext
 def teardown_db(exception):
     """This method is called after each request to close the storage"""
     storage.close()
+
 
 if __name__ == "__main__":
     import os
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
     app.run(host=host, port=port, threaded=True)
-
