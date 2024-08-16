806a87c6bcce8ee82955d8ffc9e92f128320aacc
x33zp <zubbypeculiar@gmail.com>
2024-08-16 14:53:24 +0100

    pycodestyle fix

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -43,7 +43,8 @@ def delete_city(city_id):
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
 def create_city(state_id):
     """Creates a state object"""
     state = storage.get(State, state_id)
