8e61b4667958cb51630e867dfadfcc2a6c34c01b
x33zp <zubbypeculiar@gmail.com>
2024-08-16 01:43:41 +0100

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,18 +5,16 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-
 app.register_blueprint(app_views)
 
-
 @app.teardown_appcontext
-def close_db():
-    """close db"""
+def teardown_db(exception):
+    """This method is called after each request to close the storage"""
     storage.close()
 
+if __name__ == "__main__":
+    import os
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = os.getenv('HBNB_API_PORT', '5000')
+    app.run(host=host, port=port, threaded=True)
 
-if __name__ == '__main__':
-    host = environ.get('HBNB_API_HOST', default='0.0.0.0')
-    port = environ.get('HBNB_API_PORT', default=5000)
-
-    app.run(host, int(port), threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,23 +1,9 @@
 #!/usr/bin/python3
 """Index"""
-from models import storage
-from flask import Flask, jsonify
 from api.v1.views import app_views
-
+from flask import jsonify
 
 @app_views.route('/status', strict_slashes=False)
 def status():
-    """Returns JSON status"""
+    """Returns a JSON status message"""
     return jsonify({"status": "OK"})
-
-
-@app_views.route('/status', strict_slashes=False)
-def count():
-    """Retrieves the number of each objects by type
-    """
-    return jsonify({"amenities": storage.count("Amenity"),
-                    "cities": storage.count("City"),
-                    "places": storage.count("Place"),
-                    "reviews": storage.count("Review"),
-                    "states": storage.count("State"),
-                    "users": storage.count("User")})
